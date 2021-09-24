ed05168d68523520dbdd922d37d32696a6273bf8
lena_kostiuk <90098711+LenaKostiuk00@users.noreply.github.com>
2021-09-25 00:17:40 +0300

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,15 @@
+print("Task 1")
+str = '1, 2, 3, 4, 5'
+print (str.isdigit( ))
+str=("Today is a good day!")
+print(str.count(" "))
+print(str.count("."))
+a="Homework"
+print(a.center(100,' '))
+print(len(a.center(100," ")))
+print(str.title())
+print(str.endswith('ing'))
+print(str.find('a'))
+print(str.split(' '))
+a = "        The Big Bang Theory           "
+print(a.strip())
